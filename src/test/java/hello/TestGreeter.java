804fd3e935390172ae0332b0fee762e33ea03aa5
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2025-01-31 02:23:36 +0530

    Update TestGreeter.java

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -17,7 +17,7 @@ public class TestGreeter {
    {
       g.setName("World");
       assertEquals(g.getName(),"World");
-      assertEquals(g.sayHello(),"Hello¥ World!");
+      assertEquals(g.sayHello(),"Hello World!");
    }
    
    @Test
